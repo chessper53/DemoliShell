bf3b779c3d512f00b0252fd7516b24c6ddc21a12
Alessio Ferretti <alessio.ferretti@bwz-rappi.ch>
2023-10-24 13:55:10 +0200

    CommandFactory Looks fine

--- DemoliShell/CommandFactory.cs
+++ DemoliShell/CommandFactory.cs
@@ -9,11 +9,31 @@ namespace DemoliShell
 {
     internal class CommandFactory
     {
-        static List<string> commands;
+        static List<Type> commands = SetCommands();
 
-        static ICommand CreateCommand(Type type)
+        public static ICommand CreateCommand(Type type)
         {
+            foreach (var commandType in commands)
+            {
+                if (commandType.IsAssignableFrom(type))
+                {
+                    return Activator.CreateInstance(commandType) as ICommand;
+                }
+            }
             return null;
+        }
+        static List<Type> SetCommands()
+        {
+            Type interfaceType = typeof(ICommand);
+
+            return AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(assembly => assembly.GetTypes())
+            .Where(type => interfaceType.IsAssignableFrom(type) && !type.IsInterface)
+            .ToList();
+        }
+        public static List<Type> GetCommands()
+        {
+            return commands;
         }
     }
 }
