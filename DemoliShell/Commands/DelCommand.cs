98466663549434539e46a9f88c3654bce0df9da5
sara <sara.hager@bwz-rappi.ch>
2023-11-02 14:14:31 +0100

    structure for delete command (without function)

--- DemoliShell/Commands/DelCommand.cs
+++ DemoliShell/Commands/DelCommand.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DemoliShell.Interfaces;
+using NUnitLite;
 
 namespace DemoliShell.Commands
 {
     public class DelCommand : ICommand
     {
         public CommandContext CommandContext { get; set; } = new CommandContext();
+
+        private string userInput;
 
         //Constructor
         public DelCommand()
@@ -24,7 +27,18 @@ namespace DemoliShell.Commands
 
         public void Execute()
         {
-            throw new NotImplementedException();
+            CommandContext.OutputWriter.WriteLine("Do you really want to delete this file? y (yes), n (no)");
+
+            userInput = Console.ReadLine();
+
+            if(userInput != null && userInput.ToLower() == "y") {
+                File.Delete("Pfad_zur_Datei"); // Ersetzen!!
+                CommandContext.OutputWriter.WriteLine("File successfully deleted.");
+            } 
+            else
+            {
+                CommandContext.OutputWriter.WriteLine("File not deleted.");
+            }
         }
     }
 }
