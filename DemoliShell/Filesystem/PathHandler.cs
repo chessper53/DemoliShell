4dbb2f4de4bc3c844c4c1fa7a753995935bf06c8
Alessio Ferretti <alessio.ferretti@bwz-rappi.ch>
2023-11-02 14:28:51 +0100

    alles bis au eat also eat also eat also wenn in root verzeichnis, kein \ also eat

--- DemoliShell/Filesystem/PathHandler.cs
+++ DemoliShell/Filesystem/PathHandler.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using DemoliShell.Filesystem;
 
 namespace DemoliShell.Filesystem
 {
     public class PathHandler
     {
+        CommandOutputWriter outputWriter = new CommandOutputWriter();
+
         public string GetFullPath(Directory currentDirectory, Drive drive)
         {
             string fullPath = "";
@@ -17,7 +22,14 @@ namespace DemoliShell.Filesystem
 
             do
             {
-                fullPath = "\\" + loopDir.Name + fullPath;
+                if(loopDir.ParentDirectory != null)
+                {
+                    fullPath = "\\" + loopDir.Name + fullPath;
+                }
+                else
+                {
+                    fullPath = loopDir.Name + fullPath;
+                }
 
                 if (loopDir.ParentDirectory != null)
                 {
@@ -34,6 +46,54 @@ namespace DemoliShell.Filesystem
             fullPath = drive.Label + ":" + fullPath;
 
             return fullPath;
+        }
+
+        public Directory GetDirectory(string path, Directory currentDir, Drive drive)
+        {
+            Directory resultDir = currentDir;
+            string pattern = @"\w{1}:";
+            bool pathexist = false;
+
+            Regex regex = new Regex(pattern);
+
+            string[] splitted = path.Split('\\');
+
+
+            foreach(string word in splitted)
+            {
+                MatchCollection matches = regex.Matches(word);
+
+                if (word.Equals(".."))
+                {
+                    if(resultDir.ParentDirectory != null)
+                    {
+                        resultDir = resultDir.ParentDirectory;
+                    }
+                }
+                else if (matches.Count > 0)
+                {
+                    resultDir = drive.RootDirectory;
+                    pathexist = true;
+                }
+                else
+                {
+                    foreach (FilesystemItem item in resultDir.FilesystemItems)
+                    {
+                        if (item.Name == word && item is Directory)
+                        {
+                            resultDir = item as Directory;
+                            pathexist = true;
+                            break;
+                        }
+                    }
+                    if (!pathexist)
+                    {
+                        outputWriter.WriteLine("Your Path is invalid!");
+                    }
+                }
+            }
+
+            return resultDir;
         }
     }
 }
