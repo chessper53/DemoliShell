867a845e599f4dc9319bc933d1372d56fb32e8fe
mcjccool <120661215+mcjccool@users.noreply.github.com>
2023-11-07 09:26:20 +0100

    Update RenCommand.cs

--- DemoliShell/Commands/RenCommand.cs
+++ DemoliShell/Commands/RenCommand.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using DemoliShell.Interfaces;
@@ -24,7 +25,49 @@ namespace DemoliShell.Commands
 
         public void Execute()
         {
-            throw new NotImplementedException();
+            
+
+            if (CommandContext.Parameters.Count != 2)
+            {
+                CommandContext.OutputWriter.WriteLine("Usage: Ren <oldName> <newName>");
+                return;
+            }
+
+            string oldName;
+            string newName;
+
+            oldName = CommandContext.Parameters[0];
+            newName = CommandContext.Parameters[1];
+
+            foreach (Filesystem.Directory item in CommandContext.ShellWorkspace.CurrentDirectory.FilesystemItems)
+            {
+                if (item.Name.Equals(oldName))
+                {
+                    int index = 1;
+                    string newFullName = newName;
+
+                    // If the new name already exists, append an index number
+                    while (CommandContext.ShellWorkspace.CurrentDirectory.FilesystemItems.Any(x => x.Name.Equals(newFullName)))
+                    {
+                        index++;
+                    }
+
+                    try
+                    {
+                        // Rename the file or folder to the new name
+                        item.Name = newFullName + "_" + index; // Aktualisieren Sie den Namen im Verzeichnis
+                        CommandContext.OutputWriter.WriteLine($"Renamed to {newFullName}");
+                    }
+                    catch (Exception ex)
+                    {
+                        CommandContext.OutputWriter.WriteLine($"Error renaming: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    CommandContext.OutputWriter.WriteLine("File or folder " + oldName + " does not exist.");
+                }
+            }
         }
     }
 }
